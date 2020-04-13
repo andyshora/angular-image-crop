4a47439dcb82541f15b0ce6389762522e73445bd
Benoit Lavenier <benoit.lavenier@e-is.pro>
2020-04-13 16:41:26 +0200

    Keep EXIF class uncompress

--- Gruntfile.js
+++ Gruntfile.js
@@ -30,7 +30,7 @@ module.exports = function (grunt) {
           warnings: true,
           compress: true,
           mangle: {
-            reserved: ['BinaryFile']
+            reserved: ['BinaryFile', 'EXIF']
           },
           output: {
             beautify: false,
